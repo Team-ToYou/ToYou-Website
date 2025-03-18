efbb189a94e66b304fa5b0bcd38edac737e9871d
taeyeonyoo <taeyeonyoo16@gmail.com>
2025-03-19 00:13:11 +0900

    feat: 헤더, 푸터 구조 세팅(#4)

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,24 @@ import Home from './pages/Home'
 import TeamToYou from './pages/TeamToYou'
 import History from './pages/History'
 import Notfound from './pages/Notfound'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
-    <>
-    <Routes>
-      <Route path='/' element={<Home/>} />
-      <Route path='/teamtoyou' element={<TeamToYou/>}/>
-      <Route path='/history' element={<History/>} />
-      <Route path='*' element={<Notfound/>} />
-    </Routes>
-    </>
+    <div className='App'>
+      <Header/>
+      <div className='content'>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/teamtoyou' element={<TeamToYou/>}/>
+          <Route path='/history' element={<History/>} />
+          <Route path='*' element={<Notfound/>} />
+        </Routes>
+      </div>
+      <Footer/>
+    </div>
   )
 }
 
