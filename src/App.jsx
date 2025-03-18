f4c27b5d1a00b934ed28c427915d9b4a9aff6504
taeyeonyoo <taeyeonyoo16@gmail.com>
2025-03-19 04:05:23 +0900

    refactor: ESLint와 Prettier 규칙에 맞게 기존 파일 수정(#5)

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,27 @@
-import './App.css'
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import TeamToYou from './pages/TeamToYou'
-import History from './pages/History'
-import Notfound from './pages/Notfound'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import TeamToYou from './pages/TeamToYou';
+import History from './pages/History';
+import Notfound from './pages/Notfound';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
-
   return (
-    <div className='App'>
-      <Header/>
-      <div className='content'>
+    <div className="App">
+      <Header />
+      <div className="content">
         <Routes>
-          <Route path='/' element={<Home/>} />
-          <Route path='/teamtoyou' element={<TeamToYou/>}/>
-          <Route path='/history' element={<History/>} />
-          <Route path='*' element={<Notfound/>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/teamtoyou" element={<TeamToYou />} />
+          <Route path="/history" element={<History />} />
+          <Route path="*" element={<Notfound />} />
         </Routes>
       </div>
-      <Footer/>
+      <Footer />
     </div>
-  )
+  );
 }
 
 export default App;
