891c28302972f26fce8b1aa46d24f9fdd7ca1fd9
Daemon Jung <jjjssswww1006@gmail.com>
2025-08-09 17:55:45 +0900

    feat: iOS 설치 링크 추가

--- src/utils/handleAppDownload.js
+++ src/utils/handleAppDownload.js
@@ -1,8 +1,6 @@
 const ANDROID_LINK =
   'https://play.google.com/store/apps/details?id=com.toyou.toyouandroid';
-const IOS_LINK =
-  'https://play.google.com/store/apps/details?id=com.toyou.toyouandroid';
-// ⚠️ iOS는 아직 출시 전이므로 플레이스토어 링크로 대체 (릴리즈 시 변경)
+const IOS_LINK = 'https://apps.apple.com/kr/app/%ED%88%AC%EC%9C%A0-%EC%A7%88%EB%AC%B8%EC%9C%BC%EB%A1%9C-%EC%93%B0%EB%8A%94-%EC%9D%BC%EA%B8%B0-%EA%B0%90%EC%A0%95-%EA%B8%B0%EB%A1%9D/id6742442596';
 
 const handleAppDownload = (os) => {
   if (os === 'Android') {
